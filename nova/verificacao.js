408134f00116735c4b2d77bcf102936fd5aade2f
jallinhabbei12434 <pegaasala923@gmail.com>
2025-06-05 07:33:23 -0300

    Update verificacao.js

--- nova/verificacao.js
+++ nova/verificacao.js
@@ -441,7 +441,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const numero = numeroConfirmado || userData.phone?.replace(/\D/g, "")
 
         await fetch(
-          "https://main-n8n.ohbhf7.easypanel.host/resend-code",
+          "https://main-bot.ohbhf7.easypanel.host/resend-code",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
